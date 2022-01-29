309e9ec249a762cc43534f7e9598deec47f215f9
vahid bakhtiary <vahidbakhtiaryinfo@gmail.com>
2022-01-29 01:44:57 -0500

    refactor hadoop docker

--- hadoop/start-hadoop.sh
+++ hadoop/start-hadoop.sh
@@ -6,12 +6,12 @@
 sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
-                --net=hadoop \
+                --net=hadoop-net \
                 -p 50070:50070 \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                kiwenlau/hadoop:1.0 &> /dev/null
+                vahidbakhtiaryinfo/hadoop:1.0 &> /dev/null
 
 
 # get into hadoop master container
