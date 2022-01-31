8ec007013985ac6299b398ff8ed1d6c7a580152c
vahid bakhtiary <vahidbakhtiaryinfo@gmail.com>
2022-01-31 02:37:50 -0500

    hive

---
+++ sqoop/config/sqoop-env.sh
@@ -0,0 +1,11 @@
+# Set Hadoop-specific environment variables here.
+
+#Set path to where bin/hadoop is available
+
+export HADOOP_COMMON_HOME=/usr/local/hadoop
+
+#Set path to where hadoop-*-core.jar is available
+export HADOOP_MAPRED_HOME=/usr/local/hadoop
+
+#Set the path to where bin/hive is available
+export HIVE_HOME=//usr/local/hive
